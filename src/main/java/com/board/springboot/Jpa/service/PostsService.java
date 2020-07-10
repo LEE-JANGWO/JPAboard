71fd4330cd259719e45d5254c7c43babf7aea520
LEE-JANGWO <hsrmmg10@gmail.com>
2020-07-10 20:09:30 +0900

    게시판 CRUD 처리 및 TEST코드 작성

---
+++ src/main/java/com/board/springboot/Jpa/service/PostsService.java
@@ -0,0 +1,58 @@
+package com.board.springboot.Jpa.service;
+
+import com.board.springboot.Jpa.domain.posts.Posts;
+import com.board.springboot.Jpa.domain.posts.PostsRepository;
+import com.board.springboot.Jpa.web.dto.PostsListResponseDto;
+import com.board.springboot.Jpa.web.dto.PostsResponseDto;
+import com.board.springboot.Jpa.web.dto.PostsSaveRequestDto;
+import com.board.springboot.Jpa.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+ id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다 id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/board/springboot/jpa/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package com.board.springboot.jpa.web.dto;
+
+import com.board.springboot.Jpa.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_perform_test() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
